d1bc307e358d6ed9e03ee487a22b9c49c1f3ec68
Anna Zhemchugova <mio007@mail.ru>
2022-08-22 22:35:26 +0300

    correct task13

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ if (A < 99)
 }
 if (A >= 100)
 {
-    string array = A.ToString();
-    Console.WriteLine(array[2]);
+    //string array = A.ToString();
+    //Console.WriteLine(array[2]);
+    // Нахождение третьей цифры в числе С КОНЦА:
+    int B = A % 1000;
+    int C = B / 100;
+    Console.WriteLine(C);
 }
