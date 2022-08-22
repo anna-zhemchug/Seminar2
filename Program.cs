1f2c1b4a3772f7a294ff256164e543237dddb979
Anna Zhemchugova <mio007@mail.ru>
2022-08-22 20:33:59 +0300

    task 13

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-﻿// Задача 10
-Console.Write("Введите трёхзначное число: ");
+﻿// Задача 13
+Console.Write("Введите число: ");
 int A = int.Parse(Console.ReadLine());
-int B = A % 100;
-int C = B / 10;
-Console.WriteLine($"Вторая цифра: {C}");
+if (A < 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+if (A >= 100)
+{
+    string array = A.ToString();
+    Console.WriteLine(array[2]);
+}
